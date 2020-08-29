ebdf1db1982cfc73be993fd0aef3f131ced552c3
MiguelFuquene1024 <57641730+MiguelFuquene1024@users.noreply.github.com>
2020-08-29 09:00:14 -0500

    especificación métodos

---
+++ src/main/java/hangman/model/BonusScore.java
@@ -0,0 +1,16 @@
+package hangman.model;
+
+class BonusScore implements GameScore {
+	/**
+	 * @pre correctCount: numero entero,incorrectCount:numero entero
+	 * @pos :Calcula es resultado de los puntos segun los parametros
+	 * @param correctCount:es l numero de respuestas correctas que tuvo el participante
+	 * @param incorrectCount:es el numero de respuestas incorrectas que tuvo el participante
+	 * @throws :hangman.exceptions.ExceptionValorInvalido
+	 * @return retorna el resultado de los puntos obtenidos
+	 */
+	public int calculateScore(int correctCount,int incorrectCount){
+		return 8;
+	}
+   
+}
---
+++ src/main/java/hangman/model/PowerScore.java
@@ -0,0 +1,16 @@
+package hangman.model;
+
+public class PowerScore implements GameScore{
+    /**
+     * @pre correctCount: numero entero,incorrectCount:numero entero
+     * @pos :Calcula es resultado de los puntos segun los parametros y las reglas del tipo GameScore.
+     * @param correctCount:es el numero de respuestas correctas que tuvo el participante
+     * @param incorrectCount:es el numero de respuestas incorrectas que tuvo el participante
+     * @throws :hangman.exceptions.ExceptionValorInvalido
+     * @return retorna el resultado de los puntos obtenidos
+     */
+    public int calculateScore(int correctCount,int incorrectCount){
+
+        return 8;
+        }
+}
